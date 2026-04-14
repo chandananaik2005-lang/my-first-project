1b32124e087de76d92290903ce7af19b3c5d3390
chandananaik2005-lang <chandananaik2005@gmail.com>
2026-04-14 22:03:49 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,69 @@
-public class Main {
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello Java!");
+//     }
+// }
+
+
+
+//varibles
+// public class Main{
+//     public static void main(String[] args){
+//         int a = 10;
+//         int b = 20;
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// }
+
+
+//if-else
+// public class Main{
+//     public static void main(String[] args) {
+//         int a=10;
+
+//         if(a>5){
+//             System.out.println("Big no");
+//         }
+//             else{
+//                 System.out.println("Small no");
+            
+//         }
+//     }
+// }
+
+
+
+//loop
+//   1.public class Main{
+//     public static void main(String[] args){
+//         for(int i=1;i<=5;i++){
+//             System.out.println(i);
+//         }
+
+//     }
+// }
+
+
+//2. public class Main{
+//     public static void main(String[] args)
+//     {
+//         for(int i=1;i<=10;i++)
+//         {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+//Even numbers
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        
+            for(int i=1;i<=20;i++){
+                if(i%2==0){
+                    System.out.println(i);
+                }
+            }
+        
     }
 }
